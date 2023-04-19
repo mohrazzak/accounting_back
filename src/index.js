ff62092552129fbd7714de9586346e1f28eb1910
Mohammad abdalrazak <xxiimonsteriixx20@gmail.com>
2023-04-19 17:03:59 +0300

    cookie

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ app.use(
     saveUninitialized: true,
     cookie: {
       maxAge: 1000 * 60 * 60 * 1,
-      secure: NODE_ENV === 'prod',
+      secure: NODE_ENV === 'production',
       httpOnly: false,
       sameSite: 'none',
     },
