02212d55ce2eadf966241ddfce2dfc4ca52b17f2
Mohammad abdalrazak <xxiimonsteriixx20@gmail.com>
2023-04-19 20:33:50 +0300

    adde origin

--- src/index.js
+++ src/index.js
@@ -26,22 +26,18 @@ const sessionConfig = {
     maxAge: 1000 * 60 * 60 * 1,
     // secure: NODE_ENV === 'production',
     // httpOnly: false,
-    sameSite: 'none',
+    // sameSite: 'none',
   },
+  resave: true,
+  saveUninitialized: true,
 };
-
-// if (NODE_ENV === 'production') {
-//   app.set('trust proxy', 1); // trust first proxy
-//   sessionConfig.cookie.secure = true; // serve secure cookies
-// }
-
 app.use(session(sessionConfig));
 
 app.use(morgan('dev'));
 console.log(NODE_ENV === 'production' ? PRO_URL : LOCAL_URL);
 const corsOptions = {
   credentials: true,
-  // origin: NODE_ENV === 'production' ? PRO_URL : LOCAL_URL,
+  origin: NODE_ENV === 'production' ? PRO_URL : LOCAL_URL,
 };
 app.use(cors(corsOptions));
 
