47052ee9fd6987cd0e86b85f4fb792fab142dc70
Mohammad abdalrazak <xxiimonsteriixx20@gmail.com>
2023-04-19 20:19:25 +0300

    t

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require(`express`);
 const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
-const session = require('express-session');
+const session = require('cookie-session');
 // related imports
 const routes = require('./routes');
 
