1fb9022c07d6a515d7a6cd28c8d5ec136614ae55
Mohammad abdalrazak <xxiimonsteriixx20@gmail.com>
2023-04-19 20:39:23 +0300

    adde origin

--- src/middlewares/isAuth.js
+++ src/middlewares/isAuth.js
@@ -2,6 +2,7 @@ const { StatusCodes } = require('http-status-codes');
 const { ApiError } = require('../utils/errors');
 
 module.exports = (req, res, next) => {
+  console.log(req.session);
   console.log(req.session.isAuth);
   if (req.session.isAuth) return next();
   return next(
