de24d6bb916fec4634c7a3ed1295a956cd34fc25
Mohammad Abdalrazzak <86966092+mohrazzak@users.noreply.github.com>
2023-05-10 19:23:36 +0300

    Logging daily for testing

--- src/modules/daily/daily.controllers.js
+++ src/modules/daily/daily.controllers.js
@@ -46,7 +46,7 @@ async function getAllDailyBills(req, res, next) {
       where: whereClause,
       include: [{ model: User }],
     });
-
+console.log(bills?.toJSON());
     let balance;
     if (today.isAfter(selectedDay)) balance = await getPrevBalance(selectedDay);
     else balance = await getBalance(selectedDay);
