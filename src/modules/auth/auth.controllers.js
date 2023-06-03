cda6e01dcc1318c3138e6c5657e274dd91fec88d
Mohamad Abdalrazak <xxiimonsteriixx20@gmail.com>
2023-06-03 13:30:18 +0300

    try

--- src/modules/auth/auth.controllers.js
+++ src/modules/auth/auth.controllers.js
@@ -8,10 +8,9 @@ const {
 async function login(req, res, next) {
   try {
     const { password } = req.body;
-    console.log(password);
     if (password?.toString() !== WEBSITE_PASSWORD)
       throw new ApiError('كلمةالسر غير صحيحة', StatusCodes.UNAUTHORIZED);
-    req.session.isAuth = true;
+    // req.session.isAuth = true;
     return responser(res, StatusCodes.ACCEPTED);
   } catch (error) {
     return next(error);
