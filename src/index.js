7645f44b49c82ee401b7cc7a847376c59acc6ebc
Mohamad Abdalrazak <xxiimonsteriixx20@gmail.com>
2023-06-03 13:05:30 +0300

    maybe fix error632

--- src/index.js
+++ src/index.js
@@ -47,7 +47,14 @@ const corsOptions = {
     'Access-Control-Allow-Origin',
   ],
 };
-app.use(cors({ origin: '*' }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://accounting-as.web.app');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
 app.use(routes);
 
