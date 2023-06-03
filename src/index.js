685d6c7a2b2d66d7b857fafcd888d5c7b39c7015
Mohamad Abdalrazak <xxiimonsteriixx20@gmail.com>
2023-06-03 13:26:17 +0300

    try

--- src/index.js
+++ src/index.js
@@ -58,7 +58,7 @@ app.use((req, res, next) => {
     'Access-Control-Allow-Headers',
     'X-Requested-With,content-type'
   );
-
+res.header('Access-Control-Allow-Origin', 'https://accounting-as.web.app');
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
